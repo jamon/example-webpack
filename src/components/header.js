831d738dcbce6530c03044727620698ab31e2194
jgvogt <jerry.vogt@daugherty.com>
2015-04-17 08:41:43 -0500

    Switched to Account

---
+++ src/components/header.js
@@ -0,0 +1,31 @@
+define(['external/react', 'external/react-bootstrap'], function(React, BS) {
+    return React.createClass({
+        displayName: 'account/header',
+        render: function() {
+            return <BS.Row>
+
+                <BS.Col xs={6}>
+                    Account
+                </BS.Col>
+
+                <BS.Col xs={6}>
+                    <BS.Row>
+                        <BS.Col xs={6}>
+                            <BS.DropdownButton title='Acct: 123456789 (1)' bsSize='small'>
+                                <BS.MenuItem eventKey='1'>123456789</BS.MenuItem>
+                            </BS.DropdownButton>
+                        </BS.Col>
+
+                        <BS.Col xs={6}>
+                            <BS.DropdownButton title='Tasks' bsSize='small'>
+                                <BS.MenuItem >Edit Customer</BS.MenuItem>
+                            </BS.DropdownButton>
+                        </BS.Col>
+                    </BS.Row>
+                </BS.Col>
+
+            </BS.Row>
+
+        }
+    });
+});
---
+++ src/model/account.js
@@ -0,0 +1,18 @@
+define([], function() {
+    var Model = function() {};
+
+    Model.prototype.getAccount = function(success) {
+        var req = new XMLHttpRequest();
+        req.onload = function() {
+            var response = JSON.parse(this.responseText);
+            setTimeout(function() {
+                console.log(response);
+                success(response);
+            }, 1000);
+        };
+        req.open("get", "/api/example/1/test");
+        req.send();
+    };
+
+    return (new Model());
+});
